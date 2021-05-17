697e219d84db5f08ef3faddbadd565bd36498230
Najmul-Islam <najmulislam519@gmail.com>
2021-05-18 02:10:20 +0600

    complete expressjs crush course

---
+++ routes/index.js
@@ -0,0 +1,35 @@
+const router = require('express').Router();
+const apiKeyMiddleware = require('../middlewares/apiKey')
+
+// router.use(apiKeyMiddleware)
+
+router.get('/', (req, res) => {
+    res.render('index', {
+        title: 'My home page'
+    })
+})
+
+router.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'My about page'
+    })
+})
+
+router.get('/download', (req, res) => {
+    res.download(path.resolve(__dirname) + '/img/pic.jpg');
+});
+
+// router.get('/api/products', apiKeyMiddleware, (req, res) => {
+//     res.json([
+//         {
+//             id: '123',
+//             name: 'Chrome'
+//         },
+//         {
+//             id: '124',
+//             name: 'Firefox'
+//         }
+//     ])
+// });
+
+module.exports = router;
